67b0b23f359d7810ed6ea61079a45973511b5bce
MathieuGYTB <mat.68@orange.fr>
2023-10-24 10:55:02 +0200

    replace photos, add more sass style (mixin, function, variables), and a 3D canvas

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
-import * as THREE from "https://unpkg.com/three@0.140.0/build/three.module.js";
+import * as THREE from "three";
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 const canvas = document.querySelector('#TD');
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
+  canvas: canvas,
+  alpha: true
 });
 
+const loader = new GLTFLoader();
+loader.load("./assets/r2-d2_animated/scene.gltf", (gltfScene) => {
+  //loadedModel = gltfScene
+  scene.add(gltfScene.scene);
+});
+
+const light = new THREE.PointLight(0xeeeeee);
+
+scene.add(light);
+
+camera.position.set(0, 1.5, 4);
+light.position.set(0, 4, 4);
+
+renderer.setClearColor(0xffffff, 0);
 renderer.render(scene,camera);
 
