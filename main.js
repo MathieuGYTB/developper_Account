23b708779269a643bb385589bb3a80447912bf30
MathieuGYTB <mat.68@orange.fr>
2023-10-26 14:04:48 +0200

    add function to keep clean the 3d model render

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const canvas = document.querySelector('#TD');
 
 //define variables scene, camera, renderer, light etc
 var mixer;
+var mesh;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({
@@ -19,8 +20,22 @@ scene.add(light);
 camera.position.set(0, 0, 1);
 light.position.set(0, 0, 1);
 
+function resizeRendererToDisplaySize(renderer) {
+  const canvas = renderer.domElement;
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var canvasPixelWidth = canvas.width / window.devicePixelRatio;
+  var canvasPixelHeight = canvas.height / window.devicePixelRatio;
+const needResize = canvasPixelWidth !== width || canvasPixelHeight !== height;
+  if (needResize) {
+renderer.setSize(width, height, false);
+  }
+  return needResize;
+}
+
 function animate() {
   requestAnimationFrame(animate);
+  mesh.rotation.y += 0.005;
   const clock = new THREE.Clock();
   const dt = clock.getDelta();
   if (mixer) {
@@ -31,17 +46,22 @@ function animate() {
   renderer.setClearColor(0xffffff, 0);
   renderer.setPixelRatio(devicePixelRatio);
   renderer.render(scene,camera);
+  if (resizeRendererToDisplaySize(renderer)) {
+    const canvas = renderer.domElement;
+    camera.aspect = canvas.clientWidth / canvas.clientHeight;
+    camera.updateProjectionMatrix();
+}
 };
 
 //loader 3D object from sketchfab
 const loader = new GLTFLoader();
 loader.load("./assets/r2-d2_animated.glb", (gltf) => {
-  const mesh = gltf.scene;
-  mesh.position.set(0, -0.5, 0)
-  mixer = new THREE.AnimationMixer(gltf.scene);
+  mesh = gltf.scene;
+  mesh.position.set(0, -0.5, 0);
+  scene.add(mesh);
+  mixer = new THREE.AnimationMixer(mesh);
   const animation = gltf.animations[0];
   mixer.clipAction(animation).play();
-  scene.add(mesh);
   animate();
 });
 
